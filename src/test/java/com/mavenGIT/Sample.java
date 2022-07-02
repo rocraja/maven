6a11c12dd7a0d267035aa6de60026b4e2a0478d2
rocraja <srajasekaran92@gmail.com>
2022-07-02 19:06:11 +0530

    group3

--- src/test/java/com/mavenGIT/Sample.java
+++ src/test/java/com/mavenGIT/Sample.java
@@ -1,7 +1,18 @@
 package com.mavenGIT;
 
 public class Sample {
+
+	private void rock() {
+System.out.println("rock is done");
+	}
+	private void gandhi() {
+System.out.println("gandhi done");
+	}
+	private void roman() {
+System.out.println("roman done");
+	}
 private void master() {
+	
 System.out.println("master code is write");
 }
 }
